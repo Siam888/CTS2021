aaa2384e9f61bfcbdf59c2182a25ce5afa42ef4e
Siam888 <tudorianculescu@yahoo.com>
2021-03-11 15:21:20 +0200

    Refactor metoda implementata in SUM

--- Seminar3/src/ro/ase/acs/classes/Sum.java
+++ Seminar3/src/ro/ase/acs/classes/Sum.java
@@ -5,7 +5,8 @@ import ro.ase.acs.interfaces.BinaryOperator;
 public class Sum implements BinaryOperator {
 
 	@Override
-	public double operate(double operand1, double operand2) {
+	public double operate(double operand1, double operand2)
+	{
 		return operand1 + operand2;
 	}
 
